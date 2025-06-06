25f5e55210518b4152079464a9fa862265858f9e
DESKTOP-GLVP404\Sebastian <sebastian.merca@gmail.com>
2025-06-06 18:53:19 +0300

    clean

--- external/vma/docs/html/search/pages_d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['questions_0',['Frequently asked questions',['../faq.html',1,'index']]],
-  ['quick_20start_1',['Quick start',['../quick_start.html',1,'index']]]
-];
--- include/Utils/DebugLog.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEBUG_LOG_H
-#define DEBUG_LOG_H
-
-#include <string>
-
-// Simple file logger declaration
-void LogToFile(const std::string& message);
-
-#endif // DEBUG_LOG_H
