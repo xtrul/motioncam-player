3f6e35d3afeaf2ae0cfe6d8bcae4ae5f41b06d4d
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-06-26 21:26:57 +0300

    app: auto-generate thumbnails and add preview

---
+++ include/ThumbnailGenerator.h
@@ -0,0 +1,8 @@
+#ifndef THUMBNAIL_GENERATOR_H
+#define THUMBNAIL_GENERATOR_H
+
+#include <string>
+
+bool generateThumbnail(const std::string& mcrawPath, const std::string& outPng);
+
+#endif // THUMBNAIL_GENERATOR_H
