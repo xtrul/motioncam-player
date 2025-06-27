60ae52615e501f8ce22dcd10dcd5cf0bfa9d5094
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-06-27 21:29:13 +0300

    x

--- include/Playback/PlaybackUtils.h
+++
@@ -1,8 +0,0 @@
-// This file is intentionally left empty as per the refined instructions.
-// Timestamp formatting utilities were moved to GuiUtils.
-#ifndef PLAYBACK_UTILS_H
-#define PLAYBACK_UTILS_H
-
-// No content needed based on current refactoring plan.
-
-#endif // PLAYBACK_UTILS_H
