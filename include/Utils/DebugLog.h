a248934b00f94f68a6d4153a15711dd0f4bbbeac
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-03 08:30:54 +0300

    Add dedicated ProRes export logging

--- include/Utils/DebugLog.h
+++ include/Utils/DebugLog.h
@@ -6,4 +6,7 @@
 // Simple file logger declaration
 void LogToFile(const std::string& message);
 
+// Separate logger for detailed ProRes export messages
+void LogToProResFile(const std::string& message);
+
 #endif // DEBUG_LOG_H
