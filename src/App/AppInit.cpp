82f9fecbaa50fde9ba21d23f92d4fa1ece15a1fb
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-03 13:09:08 +0300

    x

--- src/App/AppInit.cpp
+++ src/App/AppInit.cpp
@@ -145,9 +145,7 @@ App::App(const std::string& filePath) :
     m_uiAutoHidden(false),
     m_uiOpacity(1.0f),
     m_uiAutoHideDelaySec(3.0),
-    m_uiFadeSpeed(3.0f),
-    m_showExportProgressPopup(false),
-    m_proResStatus()
+    m_uiFadeSpeed(3.0f)
 {
     LogToFile(std::string("App::App Constructor called for file: ") + this->m_filePath);
 #ifndef NDEBUG
