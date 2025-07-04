0128967fa02452f5cc2b44a3008fd06f0330308a
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-04 13:19:26 +0300

    Improve HEVC export with AMF device

--- include/ffmpeg_headers.hpp
+++ include/ffmpeg_headers.hpp
@@ -3,6 +3,8 @@ extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
+#include <libavutil/opt.h>
+#include <libavutil/hwcontext.h>
 #include <libswscale/swscale.h>
 #include <libswresample/swresample.h>
 }
