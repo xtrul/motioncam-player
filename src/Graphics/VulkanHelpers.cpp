4d3fe39c19cd2bf9948d87da7c192872f28294f6
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-09 14:55:31 +0300

    xxxx

--- src/Graphics/VulkanHelpers.cpp
+++ src/Graphics/VulkanHelpers.cpp
@@ -2,9 +2,6 @@
 #include "Utils/DebugLog.h" // For LogToFile
 #include <fstream>
 #include <stdexcept> // For std::runtime_error
-#include <mutex>
-
-std::mutex g_vulkanQueueMutex; // Protect queue submissions
 
 namespace VulkanHelpers {
 
@@ -67,14 +64,10 @@ namespace VulkanHelpers {
         submitInfo.commandBufferCount = 1;
         submitInfo.pCommandBuffers = &commandBuffer;
 
-        LogToFile("[VulkanHelpers] Submitting single-time command buffer");
-        {
-            std::lock_guard<std::mutex> lock(g_vulkanQueueMutex);
-            VK_CHECK_RENDERER(vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE));
-            VK_CHECK_RENDERER(vkQueueWaitIdle(queue));
-        }
-        LogToFile("[VulkanHelpers] Command buffer completed");
+        VK_CHECK_RENDERER(vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE));
+        VK_CHECK_RENDERER(vkQueueWaitIdle(queue));
 
         vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
     }
+
 } // namespace VulkanHelpers
