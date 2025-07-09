8a0711f6742d9a8483d34f1beb61c9591439a6eb
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-09 13:06:44 +0300

    xxxx

--- include/ffmpeg_headers.hpp
+++ include/ffmpeg_headers.hpp
@@ -4,8 +4,6 @@ extern "C" {
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
 #include <libavutil/opt.h>
-#include <libavutil/hwcontext.h>
-#include <libavutil/hwcontext_vulkan.h>
 #include <libswscale/swscale.h>
 #include <libswresample/swresample.h>
 }
