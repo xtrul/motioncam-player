28bb124c46fe715bba608526353a2ef77d6a25ee
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-10 19:30:10 +0300

    Temporarily disable GPU export

--- src/Graphics/VulkanHelpers.cpp
+++ src/Graphics/VulkanHelpers.cpp
@@ -2,8 +2,10 @@
 #include "Utils/DebugLog.h" // For LogToFile
 #include <fstream>
 #include <stdexcept> // For std::runtime_error
+#include <mutex>
 
 namespace VulkanHelpers {
+std::mutex gVkSubmitMutex;
 
     std::vector<char> readFile(const std::string& filename) {
         std::string fullPath = filename;
@@ -64,8 +66,11 @@ namespace VulkanHelpers {
         submitInfo.commandBufferCount = 1;
         submitInfo.pCommandBuffers = &commandBuffer;
 
-        VK_CHECK_RENDERER(vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE));
-        VK_CHECK_RENDERER(vkQueueWaitIdle(queue));
+        {
+            std::lock_guard<std::mutex> lk(gVkSubmitMutex);
+            VK_CHECK_RENDERER(vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE));
+            VK_CHECK_RENDERER(vkQueueWaitIdle(queue));
+        }
 
         vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
     }
