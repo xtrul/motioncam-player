6600313cba1068d8c5ab234799cc72fd7a7ba12c
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-10 08:49:52 +0300

    xxx

--- src/App/AppIO.cpp
+++ src/App/AppIO.cpp
@@ -1490,10 +1490,10 @@ void App::exportCurrentClipToProRes() {
                 for (int y=0; y<height; ++y) {
                     for (int x=0; x<width/2; ++x) {
                         size_t srcIdx = static_cast<size_t>(y) * (width/2) * 4 + x*4;
-                        uint16_t y0 = gpuBuf[srcIdx];
-                        uint16_t y1 = gpuBuf[srcIdx+1];
-                        uint16_t u  = gpuBuf[srcIdx+2];
-                        uint16_t v  = gpuBuf[srcIdx+3];
+                        uint16_t y0 = gpuBuf[srcIdx] >> 6;
+                        uint16_t y1 = gpuBuf[srcIdx+1] >> 6; // Y1 directly
+                        uint16_t u  = gpuBuf[srcIdx+2] >> 6;
+                        uint16_t v  = gpuBuf[srcIdx+3] >> 6;
                         yPlane[y*frame->linesize[0]/2 + 2*x] = y0;
                         yPlane[y*frame->linesize[0]/2 + 2*x+1] = y1;
                         uPlane[y*frame->linesize[1]/2 + x] = u;
