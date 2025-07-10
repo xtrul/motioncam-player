90cc09a5216bfc3e411bbeebe60f7571d5f588b0
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-10 13:39:02 +0300

    xxx

--- src/Graphics/VulkanHelpers.cpp
+++ src/Graphics/VulkanHelpers.cpp
@@ -2,10 +2,8 @@
 #include "Utils/DebugLog.h" // For LogToFile
 #include <fstream>
 #include <stdexcept> // For std::runtime_error
-#include <mutex>
 
 namespace VulkanHelpers {
-std::mutex gVkSubmitMutex;
 
     std::vector<char> readFile(const std::string& filename) {
         std::string fullPath = filename;
@@ -66,11 +64,8 @@ std::mutex gVkSubmitMutex;
         submitInfo.commandBufferCount = 1;
         submitInfo.pCommandBuffers = &commandBuffer;
 
-        {
-            std::lock_guard<std::mutex> lk(gVkSubmitMutex);
-            VK_CHECK_RENDERER(vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE));
-            VK_CHECK_RENDERER(vkQueueWaitIdle(queue));
-        }
+        VK_CHECK_RENDERER(vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE));
+        VK_CHECK_RENDERER(vkQueueWaitIdle(queue));
 
         vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
     }
