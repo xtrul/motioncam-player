a404781ff7a622295e9fbaa490520007cf3ac215
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-11 16:36:55 +0300

    Implement GPU YUV422 unpack helper

--- include/ffmpeg_headers.hpp
+++ include/ffmpeg_headers.hpp
@@ -4,6 +4,7 @@ extern "C" {
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
 #include <libavutil/opt.h>
+#include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
 #include <libswresample/swresample.h>
 }
