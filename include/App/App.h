0954fc478105bfb7894cdccc0e1c116ce445e04e
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-17 11:56:14 +0300

    Embed preview playback in ImGui

--- include/App/App.h
+++ include/App/App.h
@@ -67,6 +67,7 @@ public:
     VkQueue m_graphicsQueue = VK_NULL_HANDLE;
     VkQueue m_presentQueue = VK_NULL_HANDLE;
     VkDescriptorPool m_imguiDescriptorPool = VK_NULL_HANDLE;
+    VkDescriptorSet m_previewTextureSet = VK_NULL_HANDLE;
     VkCommandPool m_commandPool = VK_NULL_HANDLE;
     VkRenderPass m_renderPass = VK_NULL_HANDLE;
 
