4d72757612fe4749c68fe9e296f82ac3eda5a251
Sebastian Merca <37452237+xtrul@users.noreply.github.com>
2025-07-27 14:33:20 +0300

    Use variable gradient demosaic everywhere

--- include/App/App.h
+++ include/App/App.h
@@ -81,6 +81,7 @@ public:
     std::vector<std::string> m_fileList;
     int m_currentFileIndex = -1;
     std::optional<int> m_cfaOverride;
+    int m_demosaicMode = 1;
     std::string m_cfaStringFromMetadata;
     bool m_showMetrics = false;
     bool m_showHelpPage = false;
